74718a46e55d60726d663c68fbd5c506faef58bb
jad3d <alexbobre2007@gmail.com>
2026-02-19 20:14:19 +0300

    init cmake project

--- main.cpp
+++ main.cpp
@@ -12,6 +12,5 @@ int main() {
     }
 
     return 0;
-    лев пидор
     // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
